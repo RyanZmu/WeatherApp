5e3217004f4e59d16d7c07c76e4b821abfc263b8
RyanZmu <ryanzmudka@gmail.com>
2022-01-25 19:11:18 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,139 @@
+'use strict'
+
+// Geolocation tests
+
+// let userLocation = navigator.geolocation.getCurrentPosition(x => console.log(x))
+
+//gives us lat,long and accuracy. also a timestamp.
+
+// NWS endpoint tests
+
+// https://api.weather.gov/   endpoint
+
+// https://api.weather.gov/gridpoints/{wfo}/{x},{y}/forecast  we need office ID (WFO) and then coords
+
+// https://api.weather.gov/points/42.8250496,-85.5303134  -- give an object and a URL for the forecast that we can run through a fetch and then display 
+
+const weatherDisplay = document.querySelector('.weatherDisplay')
+const displayCurrent = document.querySelector('.displayCurrent')
+const displayForecast = document.querySelector('.displayForecast')
+const backgroundImage = document.querySelector('.backgroundContainer')
+
+
+class UserWeather {
+    constructor(lat,long) {
+        this.lat = lat
+        this.long = long
+        this.userCity;
+        this.UserState;
+    }
+
+    buildUser() {
+        let userLat = this.lat
+        let userLong = this.long
+
+        fetch(`https://api.weather.gov/points/${userLat},${userLong}`)
+        .then(response => response.json())
+        .then(locationData => {
+            this.fetchForecastData(locationData.properties.forecast)
+
+            this.userCity = locationData.properties.relativeLocation.properties.city
+
+            this.UserState = locationData.properties.relativeLocation.properties.state
+        })
+    }
+
+    fetchForecastData(forecastUrl) {
+        fetch(forecastUrl)
+        .then(response => response.json())
+        .then(forecastData => {
+            this.displayWeather(forecastData)
+            console.log(forecastData)
+            // this.changeBackground(forecastData)
+        })
+    }
+
+    displayWeather(forecastData) {
+
+        console.log(forecastData.properties.periods) //an array of 
+        // objects for the time of day, first index is current forecast.
+
+        let forecastPeriods = forecastData.properties.periods
+
+            //varibles will be arrays
+            const temperatureDisplay = forecastPeriods.map(weatherData => weatherData.temperature+weatherData.temperatureUnit)
+           
+            const dayName = forecastPeriods.map(weatherData => weatherData.name)
+           
+            const forecastDisplayShort = forecastPeriods.map(weatherData => weatherData.shortForecast)
+           
+            const forecastDisplayLong = forecastPeriods.map(weatherData => weatherData.detailedForecast)
+           
+            const windSpeed = forecastPeriods.map(weatherData =>
+                weatherData.windSpeed)
+           
+            const windDirection = forecastPeriods.map(weatherData => weatherData.windDirection)
+           
+            const weatherIcon = forecastPeriods.map(weatherData => weatherData.icon)
+
+            //logic for current weather display
+            displayCurrent.append(`
+            ${dayName[0]}: ${temperatureDisplay[0]}
+            `)
+            displayCurrent.append(forecastDisplayShort[0])
+
+            const weatherIconDisplay = document.createElement('img')
+            weatherIconDisplay.src = `${weatherIcon[0]}`
+            displayCurrent.prepend(weatherIconDisplay)
+    
+            const locationHeader = document.createElement('h4')
+            locationHeader.innerHTML = `${this.userCity},${this.UserState}
+            Winds:${windDirection[0]} ${windSpeed[0]}`
+            displayCurrent.prepend(locationHeader)
+            //
+
+            //logic for 14 day forecast
+            for(let index = 1;index<forecastPeriods.length;index++){
+                const forecastCell = document.createElement('div')
+                forecastCell.classList.add('dailyForecast')
+                displayForecast.prepend(forecastCell)
+
+                const forecastIcon = document.createElement('img')
+                forecastIcon.classList.add('forecastIcon')
+                forecastIcon.src = weatherIcon[index]
+            
+                forecastCell.innerHTML = `${dayName[index]} 
+                ${temperatureDisplay[index]} ${forecastDisplayShort[index]}`
+                forecastCell.prepend(forecastIcon)
+            }
+
+             //logic for changing background based on current conditions
+        const images = [
+            `./images/snowyBG.jpg`,
+            `./images/sunnyBG.jpg`,
+            './images/rainstormBG.jpg'
+        ]
+        
+     
+        if (forecastDisplayShort[0].includes('Sunny')) {
+            backgroundImage.style.backgroundImage = `url(${images[1]})`
+        }
+        if (forecastDisplayShort[0].includes('Showers')) {
+            backgroundImage.style.backgroundImage = `url(${images[2]})`
+        }
+        if (forecastDisplayShort[0].includes('Snow Showers')){
+            backgroundImage.style.backgroundImage = `url(${images[0]})`
+        }
+        // "url(" + images[x] + ")";
+        // })
+    }
+
+    weatherButton() {
+
+    }
+}
+
+// Make a forecast class and break out the logic
+navigator.geolocation.getCurrentPosition(coords => new UserWeather(coords.coords.latitude,coords.coords.longitude).buildUser())
+
+// new UserWeather(26.12231,-80.14338).buildUser() //Fort Lauderdale weather for testing other areas weathers.
---
+++ readme.md
@@ -0,0 +1,32 @@
+**Purpose of Project**
+
+- Grab a users geolocation (use geolocation API) and display the current weather,the 5 or 7 day forecast with as much details as possible and use pictures to visualize the weather.
+
+**Planning**
+
+- Use geolocation API to get informaton on the users location, feed that to the NWS API. Figure out how the API works and feed it the LAT and LONG.
+
+- Display a picure based on the users current weather (sunny,cloudy,rainy,etc) while also having an appropiate picture of the weather in the background (if rainy have a background of a city being rain on while also displaying a smaller photo of the weather where we have our forecast)
+
+- Research some more free APIs that could enhance the users experience, possibly allow a radar/doppler.
+
+- When the user sees their current weather they should also see their forecast right below (reference drawings) and from there they can click and if the API allows it, they'll see details for that days forecast.
+
+- Make this site mobile friendly also possibly recreate this with React as we get more into that course.
+
+**Credit**
+<details>
+ - add later to code and have appear in bottom corner of website. 
+
+snowy-
+Photo by <a href="https://unsplash.com/@iam_aspencer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andrew Spencer</a> on <a href="https://unsplash.com/s/photos/snowy-city?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+banner - 
+Photo by <a href="https://unsplash.com/@lukaszlada?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Łukasz Łada</a> on <a href="https://unsplash.com/s/photos/weather-banner?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+
+
+storm - 
+Photo by <a href="https://unsplash.com/@maxsaeling?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Max Saeling</a> on <a href="https://unsplash.com/s/photos/thunderstorm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+  </details>
